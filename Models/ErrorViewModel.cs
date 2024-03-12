7a91649f5de462465017a8a8c2d91368cb2c2a3b
bhedaf <bhedaf@sheridancollege.ca>
2024-03-12 14:52:45 -0400

    test commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServiceWorkerWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
