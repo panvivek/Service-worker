550c9c417eaa8f7e792fdf65dc2cc0f53183ecfe
panvivek <162628544+panvivek@users.noreply.github.com>
2024-09-27 16:02:09 -0400

    Add files via upload

---
+++ Reviews.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ServiceWorkerWebsite.Controllers;
+
+namespace ServiceWorkerWebsite.Models
+{
+	public class Reviews
+    {
+            [Key] // Indicates this is the primary key
+            public int Id { get; set; }
+       
+        [ForeignKey("Worker_Id")] // Specifies the foreign key relationship
+            public int Worker_Id { get; set; }
+
+            [Required]
+            public int RatingValue { get; set; }
+
+            public string Comment { get; set; } // Optional text comment
+
+            [Required]
+            public DateTime ReviewDate { get; set; }
+
+            // Navigation property to access the associated Worker
+            public virtual Worker Worker { get; set; }
+    }
+}
+
