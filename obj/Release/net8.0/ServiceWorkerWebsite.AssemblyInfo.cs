c4103d34749acbe584e049fbc81e393c6576c3b7
panvivek <panvivek@sheridancollege.ca>
2024-11-20 17:55:49 -0500

    Review

--- obj/Release/net8.0/ServiceWorkerWebsite.AssemblyInfo.cs
+++ obj/Release/net8.0/ServiceWorkerWebsite.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceWorkerWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaf4991fa5910e892777d25e09509bcba7aeb70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534e59163cfe1ba9594e340deea947a354e8a873")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceWorkerWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceWorkerWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
